dcadf635ed6b1817067d7db22bd721cb02221109
floflobay <fbayeux@gmail.com>
2018-04-03 23:38:15 +0200

    b

--- projet-agilite/src/test/java/AppartementTest2.java
+++ projet-agilite/src/test/java/AppartementTest2.java
@@ -1,23 +1,19 @@
 
 import static org.junit.Assert.*;
-
-
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src")
+@CucumberOptions(features="")
 public class AppartementTest2 {
 	 Piece chambre = new Piece();
 	 Piece salon;
 	 AppartementPersonnel monAppart = new AppartementPersonnel();
 	 Piece p = new Piece();
 	 
-	@Before
+/*	@Before
 	public void setUp() throws Exception {
 		p = new Piece();
 		chambre = new Piece("chambre", 12.0,monAppart);
@@ -33,7 +29,7 @@ public class AppartementTest2 {
 		assertEquals(monAppart.getPiece(0),chambre);
 		assertEquals(monAppart.prixAppart(10.0),390.0,0.1);
 		verifierAssociation();
-	}
+	}*/
 
 	public void construireNomPieceTest(String nom){
 		p.setNomPiece(nom);
